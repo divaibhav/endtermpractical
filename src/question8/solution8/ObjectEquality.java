c8ffe76c33479bddd1fec3bc72f217383b1d64e0
divaibhav <divaibhav@gmail.com>
2019-11-26 17:39:02 +0530

    create and implemented main class

--- src/question8/solution8/ObjectEquality.java
+++ src/question8/solution8/ObjectEquality.java
@@ -7,4 +7,15 @@
 package question8.solution8;
 
 public class ObjectEquality {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Ramesh");
+        s1.setRollNo(13);
+        s1.setCpi(7.32);
+
+        Student s2 = new Student();
+        //comparing s1 and s2 using equals
+        boolean result = s1.equals(s2);
+        System.out.println(result);
+    }
 }
